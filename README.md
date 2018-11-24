220cad2ca370465ed61da1066380cd75d1c8cf71
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-11-24 15:33:12 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test for controlling thermal printer from M5Stack
+
+https://cdn-learn.adafruit.com/downloads/pdf/mini-thermal-receipt-printer.pdf
---
+++ src/A_printertest.ino
@@ -0,0 +1,134 @@
+/*------------------------------------------------------------------------
+  Example sketch for Adafruit Thermal Printer library for Arduino.
+  Demonstrates a few text styles & layouts, bitmap printing, etc.
+
+  IMPORTANT: DECLARATIONS DIFFER FROM PRIOR VERSIONS OF THIS LIBRARY.
+  This is to support newer & more board types, especially ones that don't
+  support SoftwareSerial (e.g. Arduino Due).  You can pass any Stream
+  (e.g. Serial1) to the printer constructor.  See notes below.
+
+  You may need to edit the PRINTER_FIRMWARE value in Adafruit_Thermal.h
+  to match your printer (hold feed button on powerup for test page).
+  ------------------------------------------------------------------------*/
+
+#include "M5Stack.h"
+#include "Adafruit_Thermal.h"
+#include "adalogo.h"
+#include "adaqrcode.h"
+
+// Here's the new syntax when using SoftwareSerial (e.g. Arduino Uno) ----
+// If using hardware serial instead, comment out or remove these lines:
+
+// #include "SoftwareSerial.h"
+// #define TX_PIN 17 // Arduino transmit  YELLOW WIRE  labeled RX on printer
+// #define RX_PIN 16 // Arduino receive   GREEN WIRE   labeled TX on printer
+
+// SoftwareSerial mySerial(RX_PIN, TX_PIN); // Declare SoftwareSerial obj first
+Adafruit_Thermal printer(&Serial);     // Pass addr to printer constructor
+// Then see setup() function regarding serial & printer begin() calls.
+
+// Here's the syntax for hardware serial (e.g. Arduino Due) --------------
+// Un-comment the following line if using hardware serial:
+
+//Adafruit_Thermal printer(&Serial1);      // Or Serial2, Serial3, etc.
+
+// -----------------------------------------------------------------------
+
+void setup() {
+
+  // This line is for compatibility with the Adafruit IotP project pack,
+  // which uses pin 7 as a spare grounding point.  You only need this if
+  // wired up the same way (w/3-pin header into pins 5/6/7):
+  // pinMode(7, OUTPUT); digitalWrite(7, LOW);
+
+  M5.begin(true, false, false);
+  // NOTE: SOME PRINTERS NEED 9600 BAUD instead of 19200, check test page.
+  // Serial2.begin(unsigned long baud, uint32_t config, int8_t rxPin, int8_t txPin, bool invert)
+  Serial.begin(9600);
+  Serial.flush();
+  delay(50);
+  //Serial1.begin(19200); // Use this instead if using hardware serial
+  // printer.setCharset(CHARSET_JAPAN);
+  printer.setDefault(); // Restore printer to defaults
+  printer.begin();        // Init printer (same regardless of serial type)
+
+
+  delay(1000);
+  M5.Lcd.println("printing hoge:");
+  printer.println("ほげほげ");
+  // The following calls are in setup(), but don't *need* to be.  Use them
+  // anywhere!  They're just here so they run one time and are not printed
+  // over and over (which would happen if they were in loop() instead).
+  // Some functions will feed a line when called, this is normal.
+
+  M5.Lcd.println("Inverse on & off.");
+  // Test inverse on & off
+  printer.inverseOn();
+  printer.println(F("Inverse ON"));
+  printer.inverseOff();
+
+  M5.Lcd.println("Character double-height on & off");
+  // Test character double-height on & off
+  printer.doubleHeightOn();
+  printer.println(F("Double Height ON"));
+  printer.doubleHeightOff();
+
+  M5.Lcd.println("Justification");
+  // Set text justification (right, center, left) -- accepts 'L', 'C', 'R'
+  printer.justify('R');
+  printer.println(F("Right justified"));
+  printer.justify('C');
+  printer.println(F("Center justified"));
+  printer.justify('L');
+  printer.println(F("Left justified"));
+
+  M5.Lcd.println("More styles");
+  // Test more styles
+  printer.boldOn();
+  printer.println(F("Bold text"));
+  printer.boldOff();
+
+  printer.underlineOn();
+  printer.println(F("Underlined text"));
+  printer.underlineOff();
+
+  printer.setSize('L');        // Set type size, accepts 'S', 'M', 'L'
+  printer.println(F("Large"));
+  printer.setSize('M');
+  printer.println(F("Medium"));
+  printer.setSize('S');
+  printer.println(F("Small"));
+
+  printer.justify('C');
+  printer.println(F("normal\nline\nspacing"));
+  printer.setLineHeight(50);
+  printer.println(F("Taller\nline\nspacing"));
+  printer.setLineHeight(); // Reset to default
+  printer.justify('L');
+
+  M5.Lcd.println("Barcode");
+  // Barcode examples:
+  // CODE39 is the most common alphanumeric barcode:
+  printer.printBarcode("ADAFRUT", CODE39);
+  printer.setBarcodeHeight(100);
+  // Print UPC line on product barcodes:
+  printer.printBarcode("123456789123", UPC_A);
+
+  M5.Lcd.println("Logo");
+  // Print the 75x75 pixel logo in adalogo.h:
+  printer.printBitmap(adalogo_width, adalogo_height, adalogo_data);
+
+  // Print the 135x135 pixel QR code in adaqrcode.h:
+  printer.printBitmap(adaqrcode_width, adaqrcode_height, adaqrcode_data);
+  printer.println(F("Adafruit!"));
+  printer.feed(2);
+
+  M5.Lcd.println("Reset");
+  printer.sleep();      // Tell printer to sleep
+  delay(3000L);         // Sleep for 3 seconds
+  printer.wake();       // MUST wake() before printing again, even if reset
+  printer.setDefault(); // Restore printer to defaults
+}
+
+void loop() {
+}
